d1e5a1525839f042180681569abf5a6c2dd0827d
Jochen <jo@hassfurter.com>
2018-08-02 12:55:46 +0200

    TODO: - home.component.ts - recode Mockis Login! - check if the ...-dialog.component.html and ...-dialog.component.ts which were removed with the jhipster 5 upgrade really aren't necessary any more

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
-import {JhiEventManager, JhiAlertService} from 'ng-jhipster';
+import { Component, OnInit } from '@angular/core';
+import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { JhiEventManager } from 'ng-jhipster';
 
-import {LoginModalService, Principal, Account} from 'app/core';
+import { LoginModalService, Principal, Account } from 'app/core';
 
 @Component({
     selector: 'jhi-home',
@@ -10,15 +10,10 @@ import {LoginModalService, Principal, Account} from 'app/core';
     styleUrls: ['home.scss']
 })
 export class HomeComponent implements OnInit {
-    authenticationError: boolean;
     account: Account;
     modalRef: NgbModalRef;
-    name: string;
-    vorname: string;
-    code: string;
 
-    constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager) {
-    }
+    constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager) {}
 
     ngOnInit() {
         this.principal.identity().then(account => {
@@ -35,40 +30,11 @@ export class HomeComponent implements OnInit {
         });
     }
 
-    parseLogin() {
-        this.loginService.login({
-            username: this.vorname + '.' + this.name,
-            password: this.code,
-            rememberMe: false
-        }).then(() => {
-
-            this.authenticationError = false;
-            // this.activeModal.dismiss('login success');
-            if (this.router.url === '/register' || (/^\/activate\//.test(this.router.url)) ||
-                (/^\/reset\//.test(this.router.url))) {
-                this.router.navigate(['']);
-            }
-
-            this.eventManager.broadcast({
-                name: 'authenticationSuccess',
-                content: 'Sending Authentication Success'
-            });
-
-            // // previousState was set in the authExpiredInterceptor before being redirected to login modal.
-            // // since login is succesful, go to stored previousState and clear previousState
-            const redirect = this.stateStorageService.getUrl();
-            if (redirect) {
-                this.stateStorageService.storeUrl(null);
-                this.router.navigate([redirect]);
-            }
-        }).catch(() => {
-            this.authenticationError = true;
-        });
-
-    }
-
     isAuthenticated() {
         return this.principal.isAuthenticated();
     }
 
+    login() {
+        this.modalRef = this.loginModalService.open();
+    }
 }
