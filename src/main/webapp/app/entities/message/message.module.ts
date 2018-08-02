e48d1ceb0ef182341202eb87842f8b38622acced
Jochen Haßfurter <jo@hassfurter.com>
2018-08-02 11:06:21 +0200

    Merge branch 'jhipster_upgrade' into primeng-module
    Conflicts:
    	build.gradle
    	gradle/wrapper/gradle-wrapper.properties
    	package.json
    	src/main/docker/postgresql.yml
    	src/main/java/rocks/gebsattel/hochzeit/domain/AllowControl.java
    	src/main/java/rocks/gebsattel/hochzeit/domain/Message.java
    	src/main/java/rocks/gebsattel/hochzeit/repository/AllowControlRepository.java
    	src/main/java/rocks/gebsattel/hochzeit/repository/MessageRepository.java
    	src/main/java/rocks/gebsattel/hochzeit/service/AllowControlService.java
    	src/main/java/rocks/gebsattel/hochzeit/service/UserService.java
    	src/main/java/rocks/gebsattel/hochzeit/service/impl/AllowControlServiceImpl.java
    	src/main/java/rocks/gebsattel/hochzeit/service/impl/MessageServiceImpl.java
    	src/main/java/rocks/gebsattel/hochzeit/service/impl/PartyFoodServiceImpl.java
    	src/main/java/rocks/gebsattel/hochzeit/service/impl/UserExtraServiceImpl.java
    	src/main/resources/config/liquibase/changelog/20180616143744_added_entity_UserExtra.xml
    	src/main/webapp/app/account/register/register.component.html
    	src/main/webapp/app/entities/allow-control/allow-control-delete-dialog.component.html
    	src/main/webapp/app/entities/allow-control/allow-control-detail.component.html
    	src/main/webapp/app/entities/allow-control/allow-control-dialog.component.html
    	src/main/webapp/app/entities/allow-control/allow-control-dialog.component.ts
    	src/main/webapp/app/entities/allow-control/allow-control.component.html
    	src/main/webapp/app/entities/allow-control/allow-control.module.ts
    	src/main/webapp/app/entities/message/message-delete-dialog.component.html
    	src/main/webapp/app/entities/message/message-detail.component.html
    	src/main/webapp/app/entities/message/message-dialog.component.html
    	src/main/webapp/app/entities/message/message-dialog.component.ts
    	src/main/webapp/app/entities/message/message.component.html
    	src/main/webapp/app/entities/message/message.module.ts
    	src/main/webapp/app/entities/party-food/party-food-detail.component.html
    	src/main/webapp/app/entities/party-food/party-food-dialog.component.html
    	src/main/webapp/app/entities/party-food/party-food-dialog.component.ts
    	src/main/webapp/app/entities/party-food/party-food.component.html
    	src/main/webapp/app/entities/user-extra/user-extra-detail.component.html
    	src/main/webapp/app/entities/user-extra/user-extra.component.html
    	src/main/webapp/app/home/home.component.html
    	src/main/webapp/app/home/home.component.ts
    	src/main/webapp/app/home/home.scss
    	src/main/webapp/app/layouts/navbar/navbar.component.html
    	src/main/webapp/content/scss/_bootstrap-variables.scss
    	src/main/webapp/content/scss/global.scss

--- src/main/webapp/app/entities/message/message.module.ts
+++ src/main/webapp/app/entities/message/message.module.ts
@@ -1,9 +1,9 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AccordionModule, AutoCompleteModule } from 'primeng/primeng';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AccordionModule, AutoCompleteModule} from 'primeng/primeng';
 
-import { WeddingplanerSharedModule } from 'app/shared';
+import {WeddingplanerSharedModule} from 'app/shared';
 
 import {
     MessageComponent,
